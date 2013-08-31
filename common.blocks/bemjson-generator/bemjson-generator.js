9fc9f7ff7bbf8e625a9c4cc56588dc12539fecdc
Sergey Maksimov <smaksimoff@gmail.com>
2013-08-31 22:19:22 +0400

    Generate html by bemjson

--- common.blocks/bemjson-generator/bemjson-generator.js
+++ common.blocks/bemjson-generator/bemjson-generator.js
@@ -35,6 +35,7 @@
                 }
             };
 
+        // ctx
         bemgen.ctx = function(params) {
             extend(_ctx, params);
 
@@ -47,7 +48,7 @@
         });
 
         // extenders
-        ['mods', 'elemMods', 'mix', 'attrs'].forEach(function(name) {
+        ['mods', 'elemMods', 'attrs'].forEach(function(name) {
             bemgen[name] = getPropExtender(name);
         });
 
@@ -63,8 +64,114 @@
         };
 
         // content
-        bemgen.content = function() {
-            return bemgen.apply(this, arguments);
+        bemgen.content = function(content) {
+            _ctx.content = arguments.length > 1 ? Array.prototype.slice.call(arguments) : content;
+
+            return bemgen;
+        };
+
+        // html
+        bemgen.html = function(node, block, elem) {
+            node = node || _ctx;
+            var nodeType = typeof node,
+                buf = [];
+
+            if (nodeType == 'string' || nodeType == 'number') {
+                buf.push(node);
+            } else {
+                if (Array.isArray(node)) {
+                    buf.push(node.map(function(item) {
+                        return bemgen.html(item, block, elem)
+                    }).join(''));
+                } else {
+                    var SHORT_TAGS = {
+                            area: 1, base: 1, br : 1, col : 1, command : 1, embed : 1, hr : 1, img : 1,
+                            input : 1, keygen : 1, link : 1, meta : 1, param : 1, source : 1, wbr : 1
+                        },
+                        ELEM_DELIMETER = '__',
+                        MOD_DELIMETER = '_',
+                        buildClass = function(block, elem, modName, modVal) {
+                            if (typeof modVal == 'undefined' && typeof modName != 'undefined') {
+                                modVal = modName;
+                                modName = elem;
+                                elem = undefined;
+                            }
+
+                            return block +
+                                (elem ? ELEM_DELIMETER + elem: '') +
+                                (modName ? MOD_DELIMETER + modName + MOD_DELIMETER + modVal: '');
+                        },
+                        buildClasses = function(block, elem, mods) {
+                            var res = [buildClass(block, elem)];
+
+                            if (mods) {
+                                for (var modName in mods)
+                                    if (mods.hasOwnProperty(modName))
+                                        res.push(buildClass(block, elem, modName, mods[modName]));
+                            }
+
+                            return res.join(' ');
+                        },
+                        block = node.block || block,
+                        elem = node.elem || elem,
+                        tag = node.tag || 'div',
+                        cls = node.cls || '',
+                        jsParams,
+                        mixClasses = '';
+
+                    buf.push('<', tag);
+
+                    if (node.js) {
+                        jsParams = {};
+                        jsParams[buildClass(block, elem)] = node.js === true ? {} : node.js;
+                    }
+
+                    if (node.mix) {
+                        mixClasses = ' ';
+                        var mix = Array.isArray(node.mix) ? node.mix : [node.mix],
+                            i = 0,
+                            l = mix.length,
+                            mixItem,
+                            mixBlock,
+                            mixElem;
+
+                        while (i < l) {
+                            mixItem = mix[i++];
+                            mixBlock = mixItem.block || block;
+                            mixElem = mixItem.elem || mixItem.block ? undefined : elem;
+
+                            mixClasses += buildClasses(
+                                mixBlock,
+                                mixElem,
+                                mixItem.mods);
+
+                            if (mixItem.js) {
+                                (jsParams || (jsParams = {}))[buildClass(mixBlock, mixElem)] = mixItem.js === true ? {} : mixItem.js;
+                            }
+                        }
+                    }
+
+                    buf.push(' class="', buildClasses(block, elem, node.mods), mixClasses, jsParams ? ' i-bem': '', '"');
+                    jsParams && buf.push(' onclick=" return ', JSON.stringify(jsParams), '"');
+
+                    if (node.attrs) {
+                        for (var attrName in node.attrs)
+                            node.attrs.hasOwnProperty(attrName) && buf.push(' ', attrName, '="', node.attrs[attrName], '"')
+                    }
+
+                    if (SHORT_TAGS[tag]) {
+                        buf.push('/>');
+                    } else {
+                        buf.push('>');
+
+                        node.content && buf.push(bemgen.html(node.content, block, elem));
+
+                        buf.push('</', tag, '>');
+                    }
+                }
+            }
+
+            return buf.join('');
         };
 
         return bemgen;
--- examples.js
+++ examples.js
@@ -1,18 +1,20 @@
 var bemgen = require('./common.blocks/bemjson-generator/bemjson-generator.js');
 
 // hack to use modules' methods as global functions
-var block, elem, ctx;
 for (var name in bemgen)
     if (bemgen.hasOwnProperty(name))
         GLOBAL[name] = bemgen[name];
 
 console.log(JSON.stringify(
     block('link')
+        .tag('a')
         .mods({ pseudo: 'yes' })
         .ctx({ url: 'ya.ru' })
+        .js(true)
         .mix(block('bla')())
         .content(
             elem('inner')('yandex'),
             '!'
         )
+        .html()
     , false, '  '));
