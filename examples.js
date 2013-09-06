1ff7171a03d96895a8ac6484647266e8e6ded309
Sergey Maksimov <smaksimoff@gmail.com>
2013-09-06 14:46:03 +0400

    some examples

--- examples.js
+++ examples.js
@@ -1,24 +1,101 @@
 var bemgen = require('./common.blocks/bemjson-generator/bemjson-generator.js');
 
-// hack to use modules' methods as global functions
-for (var name in bemgen)
-    if (bemgen.hasOwnProperty(name))
-        GLOBAL[name] = bemgen[name];
+// globals
+var ctx = bemgen['ctx'],
+    tag = bemgen['tag'],
+    block = bemgen['block'],
+    elem = bemgen['elem'],
+    b = bemgen['b'],
+    e = bemgen['e'];
+
+
+var page = block('b-page')
+    .ctx('title', 'examples')
+    .ctx('head', [
+        elem('css')
+            .ctx({
+                url: 'test.css',
+                ie: false
+            })()
+    ])
+    .mod('theme', 'normal')
+    .content(
+
+        //short form
+        b('link')
+            .e('inner')
+            .m({ valign: 'middle' })
+            ('yandex'),
+        /*{
+            block: 'link',
+            elem: 'inner',
+            mods: { valign: 'middle' },
+            content: 'yandex'
+         },*/
+
+        // link
+        block('link')
+            .tag('a')
+            .mod('pseudo', 'yes')
+            .attr('target', '_blank')
+            .ctx('url', 'ya.ru')
+            .js(true)
+            ('yandex'),
+        /*{
+            block: 'link',
+            tag: 'a',
+            mods: { pseudo: 'yes' },
+            attrs: { target: '_blank' },
+            url: 'ya.ru',
+            js: true,
+            content: 'yandex'
+        },*/
+
+        // select
+        block('select')
+            .ctx('name', 'mail')
+            .mods({ size: 'm', theme: 'normal' })
+            (
+                block('button')('Отправленные'),
+                elem('control')
+                    (
+                        elem('options')
+                            .attrs({ value: 'send' })
+                            ('Отправленные'),
+                        elem('options')
+                            .attrs({ value: 'draft' })
+                            ('Черновики')
+                    )
+            )
+        /*{
+            block: 'select',
+            name: 'mail',
+            mods: { size: 'm', theme: 'normal' },
+            content: [
+                {
+                    block: 'button',
+                    content: 'Отправленные'
+                },
+                {
+                    elem: 'control',
+                    content: [
+                        {
+                            elem: 'option',
+                            attrs: { value: 'send' },
+                            content: 'Отправленные'
+                        },
+                        {
+                            elem: 'option',
+                            attrs: { value: 'draft' },
+                            content: 'Черновики'
+                        }
+                    ]
+                }
+            ]
+        },*/
+
+    );
 
 console.log(JSON.stringify(
-    block('link')
-        .tag('a')
-        .mods({ pseudo: 'yes' })
-        .ctx({ url: 'ya.ru' })
-        .js(true)
-        .mix(block('bla')())
-        .content(
-            elem('inner')('yandex'),
-            '!'
-        )
-        .html()
-    , false, '  '));
-
-console.log(JSON.stringify(
-    b('link').e('inner').m({ valign: 'middle' }).content('yandex').html()
+    page()
     , false, '  '));
